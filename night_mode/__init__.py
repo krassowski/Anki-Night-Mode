60c06615fb41ff7aa152811546637ad23409ba3c
lovac42 <lovac42@protonmail.com>
2020-01-22 09:32:59 -0500

    replaced variable with remHook

--- night_mode/__init__.py
+++ night_mode/__init__.py
@@ -1,22 +1,20 @@
 from aqt import mw
-from anki.hooks import addHook
+from anki.hooks import addHook, remHook
 
 
 #addons should selectively load before or after a delay of 666
 NM_RESERVED_DELAY = 666
 
 
-night_mode = None
 def delayedLoader():
-    global night_mode
     from .night_mode import NightMode
     night_mode = NightMode()
     night_mode.load()
 
 def onProfileLoaded():
-    if not night_mode:
-        mw.progress.timer(
-            NM_RESERVED_DELAY, delayedLoader, False
-        )
+    remHook("profileLoaded", onProfileLoaded)
+    mw.progress.timer(
+        NM_RESERVED_DELAY, delayedLoader, False
+    )
 
 addHook('profileLoaded', onProfileLoaded)
